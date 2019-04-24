3e72fe93d50479acb963d7e6955cc99de6656ce4
Jared Draper <java2mochalatte@gmail.com>
2019-04-24 18:37:43 -0500

    Set some var values in Ship class constructor

--- source/screen/ship/Ship.hx
+++ source/screen/ship/Ship.hx
@@ -4,8 +4,19 @@ import flixel.FlxSprite;
 
 class Ship extends FlxSprite
 {
+    /**
+     * Variable for store sheild health.
+     */
+    private var _shield:Float;
+
+
     override public function new(X?:Float, Y?:Float):Void{
         super(X, Y);
+        solid = true;
+        health = 100;
+        shield = 0;
+        maxVelocity = 100;
+        velocity = 0;
     }//constructor
 
     
