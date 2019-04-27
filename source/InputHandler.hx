7812da4b7589156bcd3ff214e47bde2d7d969423
Jared Draper <java2mochalatte@gmail.com>
2019-04-26 21:36:10 -0500

    Implemented InputHandler

--- source/InputHandler.hx
+++ source/InputHandler.hx
@@ -2,6 +2,7 @@ package;
 
 import flixel.FlxBasic;
 import flixel.input.keyboard.FlxKeyboard;
+import command.*;
 
 class InputHandler extends FlxBasic;
 {
@@ -12,12 +13,57 @@ class InputHandler extends FlxBasic;
     private static var _keys():FlxKeyboard;
 
 
-    override public function new():Void{
+    /**
+     * Reference to the on-screen player ship object.
+     */
+    private var player:PlayerShip;
+
+
+    /**
+     * Vars for expressing input based on direction.
+     */
+    private var _up:Bool;
+    private var _down:Bool;
+    private var _left:Bool;
+    private var _right:Bool;
+    private var _primary:Bool;
+    private var _secondary:Bool;
+
+
+    override public function new(playerShip:PlayerShip):Void{
         super();
+        player = playerShip;
     }//constructor
 
     
     override public function update(elapsed:Float):Void{
         super(elapsed);
+        handleInput();
     }//update loop
+
+
+    /**
+     * Takes keyboard inputs from user and issues commands based on that input.
+     * Input presidence: Down > Up, Left = Right
+     */
+    public function handleInput():Void{
+        //check to see if keys are pressed
+        _up =  _keys.checkStatus(W, PRESSED);
+        _down =  _keys.checkStatus(S, PRESSED);
+        _left = _keys.checkStatus(A, PRESSED);
+        _right = _keys.checkStatus(D, PRESSED);
+        //check primary
+        //check secondary
+
+        //check for left/right key collision
+        if (_left && _right) _left = _right = false;
+        
+        //issue commands
+        if (_up) UpCommand(player);
+        if (_down) DownCommand(player);
+        if (_left) LeftCommand(player);
+        if (_right) RightCommand(player);
+        //issue primary
+        //issue secondary
+    }//function handleInput
 }
--- source/command/Command.hx
+++ source/command/Command.hx
@@ -2,5 +2,5 @@ package command;
 
 interface Command
 {
-    
+    public function execute(ship:Ship):Void;
 }
