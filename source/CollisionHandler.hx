27636631df74542e62471bb23d31e289d2f9430e
Jared Draper <java2mochalatte@gmail.com>
2019-05-11 15:37:29 -0500

    Added damage to collisions

--- source/CollisionHandler.hx
+++ source/CollisionHandler.hx
@@ -1,6 +1,7 @@
 package;
 
 import flixel.FlxBasic;
+import flixel.FlxObject;
 import flixel.FlxG;
 import flixel.group.FlxGroup;
 
@@ -12,14 +13,30 @@ class CollisionHandler extends FlxBasic
     private var _group:FlxGroup;
 
 
+    /**
+     * Damage quantity. Used to inflict damage on collision.
+     */
+    public var damage:Float;
+
+
     override function new(collisionGroup:FlxGroup):Void{
         super();
         _group = collisionGroup;
+        damage = 25;
     }//constructor
 
 
     override function update(elapsed:Float):Void{
-        FlxG.collide(_group, _group);
+        FlxG.collide(_group, _group, _collisionDamage);
         super.update(elapsed);
     }//update loop
+
+
+    /**
+     * Called on collision between two objects. Deals equal damage to both.
+     */
+    private function _collisionDamage(object1:FlxObject, object2:FlxObject):Void{
+        object1.hurt(damage);
+        object2.hurt(damage);
+    }//function _damage
 }
